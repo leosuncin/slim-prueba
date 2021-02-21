5c2288d8c6afcc6d8c10ebc055daf70344c8798b
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-02-20 21:27:48 -0600

    test: list users action
    Use JSON Schema to assert response body.

---
+++ tests/Application/Actions/Product/ListProductsActionTest.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Application\Actions\Product;
+
+use EnricoStahn\JsonAssert\Assert as JsonAssert;
+use Tests\TestCase;
+
+class ListProductsActionTest extends TestCase
+{
+    use JsonAssert;
+
+    public function testAction()
+    {
+        $request = $this->createRequest('GET', '/products');
+        $response = $this->getAppInstance()->handle($request);
+
+        $payload = (string)$response->getBody();
+        $json = json_decode($payload);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJsonMatchesSchema($json, __DIR__ . '/products-schema.json');
+        $this->assertSame($response->getStatusCode(), $json->statusCode);
+    }
+}
